bf5bc661c8f6b10560149ea8502f974dc7285b08
Antonio Terreno <antonio.terreno@gmail.com>
2014-11-05 13:52:16 +0000

    necessary changes to apigee deploy script

--- apigee.sh
+++ apigee.sh
@@ -1,1 +1,2 @@
-apigeetool deploynodeapp -n hello -m index.js -o aterreno -e test -u antonio.terreno@gmail.com -p Aappopyrrable78
+#!/bin/sh
+apigeetool deploynodeapp -d . -n hello -m server.js -o aterreno -e test -u antonio.terreno@gmail.com -p Aappopyrrable78
